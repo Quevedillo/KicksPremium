fe4c14168565d688be94e1cd577bf4542fc6ae84
jgomq <tu_email@example.com>
2026-01-21 11:58:20 +0100

    fix(main): Inicializar DateFormatting para locale es_ES

--- kickspremium_mobile/lib/main.dart
+++ kickspremium_mobile/lib/main.dart
@@ -2,11 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'presentation/router.dart';
 import 'core/theme/app_theme.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  
+  // Inicializar locale para intl
+  await initializeDateFormatting('es_ES', null);
   
   try {
     await dotenv.load(fileName: "assets/env");
